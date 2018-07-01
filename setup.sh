4f2f9c3f3ea9719c3c410eaf486959764195ab57
JohnVonNeumann <louiswillcock@gmail.com>
2018-07-01 19:29:16 +1000

    CREATE setup.sh - shell script to position dotfiles after cloning

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir ~/.config/nvim
+touch ~/.config/nvim/init.vim
+cat <<EOF > ~/.config/nvim/init.vim
+    set runtimepath^=~/.vim runtimepath+=~/.vim/after
+    let &packpath = &runtimepath
+    source ~/.vimrc
+EOF
+sudo apt-get install python-dev python-pip python3-dev python3-pip -y
+sudo apt-add-repository ppa:neovim-ppa/stable -y
+sudo apt-get update -y 
+sudo apt-get install neovim -y
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp .config/terminator/config ~/.config/terminator
+cp .vimrc ~/.vimrc
+touch ~/.bash_profile
+cp .bashrc ~/.bashrc
