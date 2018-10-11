1398b7992cc71897952b0730c569a7591c5864cf
JohnVonNeumann <louiswillcock@gmail.com>
2018-10-11 11:32:40 +1100

    UPDATE setup.sh - add in more conds to the script, test for macos, ubuntu/debian and rhel based sys', not finished, untested too.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# I don't think I've written these `elifs` correctly, I suspect the entire point of an `elif` is
+# to not contain a second else
 
 mkdir ~/.config/nvim
 touch ~/.config/nvim/init.vim
@@ -17,14 +19,32 @@ if [ -f /etc/os-release ]; then
 	else
 		echo "Yum is not installed."
 	fi
-elif [ -f /etc/
-# do the same process for ubuntu/debian based stuff"
-sudo apt-get install python-dev python-pip python3-dev python3-pip -y
-sudo apt-add-repository ppa:neovim-ppa/stable -y
-sudo apt-get update -y 
-sudo apt-get install neovim -y
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-elif [ 
-# pseudo-code 
-# if echo $OSTYPE has darwin in the string, then the os is mac, do stuff
+elif type lsb_release >/dev/null 2>&1; then
+	echo "Ubuntu/Debian is most likely installed."
+	echo "Most likely uses apt. Checking for apt."
+	if [ `which apt` ]; then
+		echo "Apt is installed."
+		# This is untested
+		# do the same process for ubuntu/debian based stuff"
+		sudo apt-get install python-dev python-pip python3-dev python3-pip -y
+		sudo apt-add-repository ppa:neovim-ppa/stable -y
+		sudo apt-get update -y 
+		sudo apt-get install neovim -y
+		curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+		    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	else 
+		echo "Apt is not installed. Perhaps the original ID was incorrect."
+	fi
+else
+	echo "Most likely not Ubuntu/Debian"
+elif [[ `echo $OSTYPE` =~ "darwin" ]]; then
+	echo "Is MacOS"
+	if [ `which brew` == 0 u then
+		echo "Brew is installed, installing other dependencies."
+		# Install Neovim stuff
+	else
+		echo "Brew is not installed. Please install then run this again."
+	fi
+else
+	echo "Not MacOS"
+fi
