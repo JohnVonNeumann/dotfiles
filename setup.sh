5052fdef812e162928794e4ea38879919ca26711
JohnVonNeumann <louiswillcock@gmail.com>
2018-10-12 11:08:23 +1100

    UPDATE setup.sh - still not tested or finished, committing changes, added completion of this to my TODOIST, and ticket has been lodged on GH

--- setup.sh
+++ setup.sh
@@ -16,6 +16,9 @@ if [ -f /etc/os-release ]; then
 		echo "Yum is installed."
 		echo "blah"
 		# install heaps of neovim stuff
+		yum -y install epel-release
+		curl -o /etc/yum.repos.d/dperson-neovim-epel-7.repo https://copr.fedorainfracloud.org/coprs/dperson/neovim/repo/epel-7/dperson-neovim-epel-7.repo 
+		yum -y install neovim
 	else
 		echo "Yum is not installed."
 	fi
