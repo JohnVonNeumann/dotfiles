8ab3b8f4e9be24023bb97cb0eb3ff0f8a4a70b0f
JohnVonNeumann <louiswillcock@gmail.com>
2018-10-20 21:45:07 +1100

    UPDATE setup.sh - add in even more unfinished stuff
    This is basically me laying out the overall shape of the script more, as
    I haven't properly just ticketed this thing up, mainly because I have
    some level of anxiety regarding actually running the thing on a live
    host and I'm considering branching and getting a CI tool to run these
    scripts on changes to test how well they run before deploying them live,
    you know, like real programming

--- setup.sh
+++ setup.sh
@@ -39,13 +39,31 @@ elif type lsb_release >/dev/null 2>&1; then
 	fi
 else
 	echo "Most likely not Ubuntu/Debian"
-elif [[ `echo $OSTYPE` =~ "darwin" ]]; then
+# echo $MACHTYPE seems like a good solution for all operating systems as it is
+# a BASH set var so it will always be found by a bash script
+elif [[ `echo $MACHTYPE` =~ "darwin" ]]; then
 	echo "Is MacOS"
-	if [ `which brew` == 0 u then
+	if [ `which brew` == 0 ] then
 		echo "Brew is installed, installing other dependencies."
 		# Install Neovim stuff
+        # what basically needs to happen here is that you do a brew install of
+        # vim with --with-python3 which will install with python3 support and
+        # enable support for vim plugins that require it ( a lot of them)
+        brew install vim --with-python3
+        # then youll need to move the new brew installed version of vim to the
+        # default location of `which vim` which also requires (i think) knowing
+        # the version of the newly installed package, this can probably be
+        # parsed out of the brew install operation
+        cp /usr/local/Cellar/vim/*/bin/vim /usr/local/bin/vim
+        # then you'll be required to install vimplug
+        curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+             https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        # then finally, run a PlugInstall to install the contents of vimrcs 
+        # plugin entry list
+        # https://stackoverflow.com/questions/13522599/how-to-run-vim-commands-from-terminal
 	else
-		echo "Brew is not installed. Please install then run this again."
+		echo "Brew is not installed. Installing."
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	fi
 else
 	echo "Not MacOS"
